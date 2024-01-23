26bdec11fedce4240adbf248414a5cd550627077
fabee <fabee@fabeenabineesh>
2024-01-23 22:28:56 +0400

    This is my first commit...

---
+++ src/ReadText.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+
+import java.util.Scanner;
+/**
+ * 
+ */
+public class ReadText {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+        System.out.println("enter the message");
+        String text=sc.nextLine();
+        System.out.println("The message is "+text);
+
+
+       System.out.println("enter your favourate number");
+       int number=sc.nextInt();
+       System.out.println("The number is "+number);
+
+
+	}
+
+}
